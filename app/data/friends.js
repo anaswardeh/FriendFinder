b3c3815222078215af65a5ad452034ca0ef7204b
anaswardeh <wardeh@un.org>
2017-11-19 11:49:42 -0500

    fixed files

---
+++ app/data/friends.js
@@ -0,0 +1,626 @@
+[
+    {
+    name: "Ahmed",
+    photo: "https://loremflickr.com/320/240/man",
+    scores: [
+    "5",
+    "1",
+    "4",
+    "4",
+    "5",
+    "1",
+    "2",
+    "5",
+    "4",
+    "1"
+    ]
+    },
+    {
+    name: "Jacob Deming",
+    photo: "https://loremflickr.com/320/240/man",
+    scores: [
+    "4",
+    "2",
+    "5",
+    "1",
+    "3",
+    "2",
+    "2",
+    "1",
+    "3",
+    "2"
+    ]
+    },
+    {
+    name: "Jeremiah Scanlon",
+    photo: "https://loremflickr.com/320/240/man",
+    scores: [
+    "5",
+    "2",
+    "2",
+    "2",
+    "4",
+    "1",
+    "3",
+    "2",
+    "5",
+    "5"
+    ]
+    },
+    {
+    name: "Louis T. Delia",
+    photo: "https://loremflickr.com/320/240/man",
+    scores: [
+    "3",
+    "3",
+    "4",
+    "2",
+    "2",
+    "1",
+    "3",
+    "2",
+    "2",
+    "3"
+    ]
+    },
+    {
+    name: "Lou Ritter",
+    photo: "https://loremflickr.com/320/240/man",
+    scores: [
+    "4",
+    "3",
+    "4",
+    "1",
+    "5",
+    "2",
+    "5",
+    "3",
+    "1",
+    "4"
+    ]
+    },
+    {
+    name: "Jordan Biason",
+    photo: "https://loremflickr.com/320/240/man",
+    scores: [
+    "4",
+    "4",
+    "2",
+    "3",
+    "2",
+    "2",
+    "3",
+    "2",
+    "4",
+    "5"
+    ]
+    },
+    {
+    name: "Michelle",
+    photo: "http://loremflickr.com/320/240/woman",
+    scores: [
+    "3",
+    "2",
+    "2",
+    "2",
+    "3",
+    "4",
+    "2",
+    "3",
+    "2",
+    "1"
+    ]
+    },
+    {
+    name: "gt",
+    photo: "http://loremflickr.com/320/240/woman",
+    scores: [
+    "1",
+    "3",
+    "3",
+    "4",
+    "5",
+    "4",
+    "1",
+    "1",
+    "5",
+    "2"
+    ]
+    },
+    {
+    name: "gt",
+    photo: "http://loremflickr.com/320/240/woman",
+    scores: [
+    "1",
+    "3",
+    "3",
+    "4",
+    "5",
+    "4",
+    "1",
+    "1",
+    "5",
+    "2"
+    ]
+    },
+    {
+    name: "rtg",
+    photo: "http://loremflickr.com/320/240/woman",
+    scores: [
+    "3",
+    "3",
+    "4",
+    "3",
+    "2",
+    "3",
+    "1",
+    "1",
+    "3",
+    "2"
+    ]
+    },
+    {
+    name: "Mohammed",
+    photo: "https://loremflickr.com/320/240/man",
+    scores: [
+    "2",
+    "4",
+    "3",
+    "2",
+    "4",
+    "3",
+    "2",
+    "4",
+    "4",
+    "5"
+    ]
+    },
+    {
+    name: "Mohammed",
+    photo: "https://loremflickr.com/320/240/man",
+    scores: [
+    "2",
+    "4",
+    "3",
+    "2",
+    "4",
+    "3",
+    "2",
+    "4",
+    "4",
+    "5"
+    ]
+    },
+    {
+    name: "Mohammed",
+    photo: "https://loremflickr.com/320/240/man",
+    scores: [
+    "2",
+    "4",
+    "3",
+    "2",
+    "4",
+    "3",
+    "2",
+    "4",
+    "4",
+    "5"
+    ]
+    },
+    {
+    name: "xyz",
+    photo: "https://loremflickr.com/320/240/man",
+    scores: [
+    "2",
+    "2",
+    "2",
+    "2",
+    "2",
+    "2",
+    "2",
+    "2",
+    "2",
+    "2"
+    ]
+    },
+    {
+    name: "james",
+    photo: "https://loremflickr.com/320/240/man",
+    scores: [
+    "3",
+    "2",
+    "2",
+    "4",
+    "3",
+    "3",
+    "4",
+    "3",
+    "5",
+    "2"
+    ]
+    },
+    {
+    name: "james",
+    photo: "https://loremflickr.com/320/240/man",
+    scores: [
+    "3",
+    "2",
+    "2",
+    "4",
+    "3",
+    "3",
+    "4",
+    "3",
+    "5",
+    "2"
+    ]
+    },
+    {
+    name: "hhh",
+    photo: "https://loremflickr.com/320/240/man",
+    scores: [
+    "2",
+    "3",
+    "1",
+    "3",
+    "3",
+    "4",
+    "5",
+    "2",
+    "5",
+    "2"
+    ]
+    },
+    {
+    name: "x",
+    photo: "https://loremflickr.com/320/240/man",
+    scores: [
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1"
+    ]
+    },
+    {
+    name: "x",
+    photo: "https://loremflickr.com/320/240/man",
+    scores: [
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1"
+    ]
+    },
+    {
+    name: "x",
+    photo: "https://loremflickr.com/320/240/man",
+    scores: [
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1"
+    ]
+    },
+    {
+    name: "xn",
+    photo: "https://loremflickr.com/320/240/man",
+    scores: [
+    "1",
+    "1",
+    "1",
+    "1",
+    "2",
+    "2",
+    "2",
+    "2",
+    "3",
+    "2"
+    ]
+    },
+    {
+    name: "xn",
+    photo: "https://loremflickr.com/320/240/man",
+    scores: [
+    "5",
+    "5",
+    "5",
+    "4",
+    "2",
+    "5",
+    "5",
+    "5",
+    "5",
+    "5"
+    ]
+    },
+    {
+    name: "test",
+    photo: "https://loremflickr.com/320/240/man",
+    scores: [
+    "2",
+    "1",
+    "1",
+    "2",
+    "1",
+    "2",
+    "2",
+    "1",
+    "1",
+    "1"
+    ]
+    },
+    {
+    name: "bogus",
+    photo: "https://loremflickr.com/320/240/man",
+    scores: [
+    "2",
+    "3",
+    "3",
+    "3",
+    "3",
+    "4",
+    "3",
+    "3",
+    "2",
+    "2"
+    ]
+    },
+    {
+    name: "M",
+    photo: "https://loremflickr.com/320/240/man",
+    scores: [
+    "4",
+    "2",
+    "2",
+    "3",
+    "4",
+    "3",
+    "2",
+    "3",
+    "3",
+    "3"
+    ]
+    },
+    {
+    name: "M",
+    photo: "https://loremflickr.com/320/240/man",
+    scores: [
+    "4",
+    "2",
+    "2",
+    "3",
+    "4",
+    "3",
+    "2",
+    "3",
+    "3",
+    "3"
+    ]
+    },
+    {
+    name: "M",
+    photo: "https://loremflickr.com/320/240/man",
+    scores: [
+    "4",
+    "2",
+    "2",
+    "3",
+    "4",
+    "3",
+    "2",
+    "3",
+    "3",
+    "3"
+    ]
+    },
+    {
+    name: "cool",
+    photo: "https://loremflickr.com/320/240/man",
+    scores: [
+    "4",
+    "2",
+    "2",
+    "3",
+    "4",
+    "2",
+    "3",
+    "3",
+    "2",
+    "3"
+    ]
+    },
+    {
+    name: "cool",
+    photo: "https://loremflickr.com/320/240/man",
+    scores: [
+    "4",
+    "2",
+    "2",
+    "3",
+    "4",
+    "2",
+    "3",
+    "3",
+    "2",
+    "3"
+    ]
+    },
+    {
+    name: "cool",
+    photo: "https://loremflickr.com/320/240/man",
+    scores: [
+    "4",
+    "2",
+    "2",
+    "3",
+    "4",
+    "2",
+    "3",
+    "3",
+    "2",
+    "3"
+    ]
+    },
+    {
+    name: "a",
+    photo: "https://loremflickr.com/320/240/man",
+    scores: [
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1"
+    ]
+    },
+    {
+    name: "Love Exists Thanks",
+    photo: "https://loremflickr.com/320/240/man",
+    scores: [
+    "1",
+    "5",
+    "4",
+    "4",
+    "4",
+    "5",
+    "3",
+    "3",
+    "3",
+    "4"
+    ]
+    },
+    {
+    name: "Holly",
+    photo: "https://loremflickr.com/320/240/man",
+    scores: [
+    "4",
+    "2",
+    "5",
+    "2",
+    "5",
+    "3",
+    "4",
+    "2",
+    "1",
+    "2"
+    ]
+    },
+    {
+    name: "Holly",
+    photo: "https://loremflickr.com/320/240/man",
+    scores: [
+    "4",
+    "2",
+    "5",
+    "2",
+    "5",
+    "3",
+    "4",
+    "2",
+    "1",
+    "2"
+    ]
+    },
+    {
+    name: "Melanie MacDonald",
+    photo: "https://loremflickr.com/320/240/man",
+    scores: [
+    "3",
+    "3",
+    "3",
+    "3",
+    "2",
+    "4",
+    "3",
+    "2",
+    "3",
+    "4"
+    ]
+    },
+    {
+    name: "b",
+    photo: "https://loremflickr.com/320/240/man",
+    scores: [
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1"
+    ]
+    },
+    {
+    name: "AA",
+    photo: "https://loremflickr.com/320/240/man",
+    scores: [
+    "1",
+    "1",
+    "1",
+    "3",
+    "2",
+    "1",
+    "5",
+    "4",
+    "2",
+    "2"
+    ]
+    },
+    {
+    name: "AA",
+    photo: "https://loremflickr.com/320/240/man",
+    scores: [
+    "1",
+    "1",
+    "1",
+    "3",
+    "2",
+    "1",
+    "5",
+    "4",
+    "2",
+    "2"
+    ]
+    },
+    {
+    name: "Ryan",
+    photo: "https://loremflickr.com/320/240/man",
+    scores: [
+    "4",
+    "4",
+    "3",
+    "1",
+    "1",
+    "2",
+    "5",
+    "1",
+    "1",
+    "1"
+    ]
+    }
+    ]
