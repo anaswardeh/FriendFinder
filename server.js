448849ee875ae675cc82f7c6c6853c8ed0fe1a72
anaswardeh <wardeh@un.org>
2017-11-18 23:42:45 -0500

    New Files

---
+++ server.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+// Configure the Express application
+var app = express();
+var PORT = process.env.PORT;
