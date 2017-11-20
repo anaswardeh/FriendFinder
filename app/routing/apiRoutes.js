b91472ea18acdb96e85bc48c86c0721fcc9d3ce5
anaswardeh <wardeh@un.org>
2017-11-19 23:54:52 -0500

    New Changes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -14,14 +14,17 @@ module.exports = function(app) {
 		res.json(friends);
 	});
 
+	
+
 	// Add new friend entry
-	app.post('/api/friends', function(req, res) {
+	app.post("/api/friends", function(req, res) {
+
 		// Capture the user input object
 		var userInput = req.body;
-		// console.log('userInput = ' + JSON.stringify(userInput));
 
+		
 		var userResponses = userInput.scores;
-		// console.log('userResponses = ' + userResponses);
+		 //console.log('userResponses = ' + userResponses);
 
 		// Compute best friend match
 		var matchName = '';
@@ -30,14 +33,12 @@ module.exports = function(app) {
 
 		// Examine all existing friends in the list
 		for (var i = 0; i < friends.length; i++) {
-			// console.log('friend = ' + JSON.stringify(friends[i]));
 
 			// Compute differenes for each question
 			var diff = 0;
 			for (var j = 0; j < userResponses.length; j++) {
 				diff += Math.abs(friends[i].scores[j] - userResponses[j]);
 			}
-			// console.log('diff = ' + diff);
 
 			// If lowest difference, record the friend match
 			if (diff < totalDifference) {
@@ -53,6 +54,7 @@ module.exports = function(app) {
 
 		// Add new user
 		friends.push(userInput);
+
 
 		// Send appropriate response
 		res.json({status: 'OK', matchName: matchName, matchImage: matchImage});
